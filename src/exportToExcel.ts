9a377b46cf4121805c87702dfab4320683ebc996
Dejan Ristic <dristic79@gmail.com>
2026-04-12 21:11:38 +0200

    parameters for script call

---
+++ src/exportToExcel.ts
@@ -0,0 +1,57 @@
+import { readFile } from 'fs/promises';
+import { parseArgs } from 'node:util';
+import ExcelJS from 'exceljs';
+import type { TranslationData } from './types/TranslationData.types.ts';
+
+const argsConfig = {
+  options: {
+    'source-file': { type: 'string' },
+    'save-as': { type: 'string' },
+  },
+} as const;
+
+const scriptArgs = parseArgs(argsConfig);
+
+const exportToExcel = (data: Buffer<ArrayBuffer>) => {
+  const translationData = JSON.parse(data.toString());
+
+  const workbook = new ExcelJS.Workbook();
+  const worksheet = workbook.addWorksheet('User Data');
+
+  worksheet.columns = [
+    { header: 'Property', key: 'prop', width: 40 },
+    { header: 'Value', key: 'val', width: 60 },
+  ];
+
+  const traverseTranslationEntries = (
+    key: string,
+    value: string | TranslationData
+  ) => {
+    if (typeof value === 'string')
+      return worksheet.addRow({ prop: key, val: value });
+    if (typeof value === 'object') {
+      Object.entries(value).map(([nestedKey, nestedValue]) =>
+        traverseTranslationEntries(`${key}.${nestedKey}`, nestedValue)
+      );
+    }
+  };
+
+  const translationEntries = Object.entries(translationData) as Array<
+    [string, string | TranslationData]
+  >;
+
+  translationEntries.forEach(([key, value]) =>
+    traverseTranslationEntries(key, value)
+  );
+
+  worksheet.getRow(1).font = { bold: true };
+  const saveName = `${scriptArgs.values['save-as']}.xlsx`;
+  workbook.xlsx.writeFile(saveName);
+};
+
+const exportFrom = `${scriptArgs.values['source-file']}.json`;
+const dataPath = new URL(exportFrom, import.meta.url);
+
+readFile(dataPath)
+  .then((data) => exportToExcel(data))
+  .catch((error) => console.error('\x1b[31m%s\x1b[0m', error));
