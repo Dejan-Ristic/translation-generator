a924919991506af1fbc8b3cba4e94a04e270a7a2
Dejan Ristic <dristic79@gmail.com>
2026-04-13 13:20:56 +0200

    creating nested props in outpus json

--- src/importFromExcel.ts
+++ src/importFromExcel.ts
@@ -1,22 +1,16 @@
 import ExcelJS from 'exceljs';
 import { writeFileSync } from 'fs';
-
-// TODO: ovo kasnije da se dinamicki uzme iz args
-const translatedlang = 'en';
+import type { TranslationData } from './types/TranslationData.types.ts';
 
 const translationError = (errorMsg = '') => {
   throw new Error(errorMsg || 'Translation not formatted');
 };
 
-async function convertColumnToJson() {
-  const workbook = new ExcelJS.Workbook();
-
-  await workbook.xlsx.readFile('./Translation-test.xlsx');
-
+const importFromExcel = (langCode: string) => {
   const worksheet = workbook.getWorksheet('translation');
   if (!worksheet) return;
 
-  const translatedObj: Record<string, string> = {};
+  const translatedObj: TranslationData = {};
 
   let propertyPathNumber = 0;
   let translatedlangNumber = 0;
@@ -28,7 +22,7 @@ async function convertColumnToJson() {
     if (rowNumber === firstRowIndex) {
       row.eachCell({ includeEmpty: false }, (cell, colNumber) => {
         if (cell.value === 'propertyPath') propertyPathNumber = colNumber;
-        if (cell.value === translatedlang) translatedlangNumber = colNumber;
+        if (cell.value === langCode) translatedlangNumber = colNumber;
       });
       if (!propertyPathNumber || !translatedlangNumber) translationError();
     } else {
@@ -36,13 +30,31 @@ async function convertColumnToJson() {
       const transVal = row.getCell(translatedlangNumber).value;
 
       if (!transProp || !transVal) translationError();
-      if (typeof transVal === 'string' && typeof transProp === 'string')
-        translatedObj[transProp] = transVal;
+
+      if (typeof transProp === 'string' && typeof transVal === 'string') {
+        const nestedProps = transProp.split('.');
+
+        let current = translatedObj;
+        nestedProps.forEach((key, index) => {
+          if (!current[key])
+            current[key] = index === nestedProps.length - 1 ? transVal : {};
+          if (typeof current[key] !== 'string') current = current[key];
+        });
+      }
     }
   });
 
   const jsonOutput = JSON.stringify(translatedObj, null, 2);
-  writeFileSync(`${translatedlang}.json`, jsonOutput);
-}
+  writeFileSync(`${langCode}.json`, jsonOutput);
+};
 
-convertColumnToJson().catch((err) => console.error(err));
+const workbook = new ExcelJS.Workbook();
+
+// TODO: ovo kasnije da se dinamicki uzme iz args
+const translatedlang = 'en';
+
+// TODO: ovo kasnije da se dinamicki uzme iz args
+workbook.xlsx
+  .readFile('./Translation-test.xlsx')
+  .then(() => importFromExcel(translatedlang))
+  .catch((error) => console.error('\x1b[31m%s\x1b[0m', error));
